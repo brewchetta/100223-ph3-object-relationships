96dd4bc98f90249f7d5d88b85f7f33ad3d745485
brewchetta <chett.tiller@gmail.com>
2023-11-16 12:07:37 -0500

    relationships yay

--- classes/models.py
+++ classes/models.py
@@ -2,11 +2,15 @@
 
 # CAR ###
 class Car:
-    
-    def __init__(self, make, model, license_plate):
+
+    all_cars = []
+
+    def __init__(self, make, model, license_plate, garage):
         self.make = make
         self.model = model
         self.license_plate = license_plate
+        self.garage = garage
+        Car.all_cars.append(self)
 
     def __repr__(self):
         return f"Car(make={self.make}, model={self.model}, license_plate={self.license_plate})"
@@ -19,7 +23,31 @@ class Garage:
 
     def __repr__(self):
         return f"Garage(address={self.address})"
-    
+
+    def filter_car(self, car):
+        return car.garage == self
+
+    def cars(self):
+        return [ car for car in Car.all_cars if car.garage == self ]
+        # return list(filter(self.filter_car, Car.all_cars))
+
+
+my_garage = Garage("Main St")
+
+pontiac = Car("Pontiac", "GTO", "123456", my_garage)
+batmobile = Car("Wayne Enterprises", "Batmobile", "JUSTICE", my_garage)
+ferrari = Car("Ferrari", "812 Superfast", "COOLGUY", my_garage)
+
+moises_garage = Garage("42nd St")
+
+pop_mobile = Car("Vatican Industries", "Popemobile", "HOLYSEE", moises_garage)
+ice_cream_truck = Car("Mr Softee", "Soft Serve", "GOODHUMOUR", moises_garage)
+tesla = Car("X", "Tesla", "X", moises_garage)
+
+
+
+
+
 ##############################
 
 
@@ -35,6 +63,16 @@ class Doctor:
 
     def __repr__(self):
         return f"Doctor(name={self.name}, specialty={self.specialty})"
+    
+    def appointments(self):
+        return [ appt for appt in Appointment.all_appointments if appt.doctor == self]
+    
+    def patients(self):
+        all_patients = [ appt.patient for appt in Appointment. all_appointments if appt.doctor == self]
+
+        uniq_patients = list( set( all_patients ) )
+
+        return uniq_patients
 
 # PATIENT ###
 class Patient:
@@ -46,9 +84,44 @@ class Patient:
     def __repr__(self):
         return f"Patient(first_name={self.first_name}, last_name={self.last_name})"
     
-##############################
+    def appointments(self):
+        return [ appt for appt in Appointment.all_appointments if appt.patient == self]
+
+class Appointment:
+
+    all_appointments = []
+
+    def __init__(self, date, doctor, patient):
+        self.date = date
+        self.doctor = doctor
+        self.patient = patient
+        Appointment.all_appointments.append(self)
+
+    def __repr__(self):
+        return f"Appoinment(date={self.date}, patient_name={self.patient.first_name} {self.patient.last_name}, doctor={self.doctor.name} )"
     
 
+p1 = Patient("John", "Doe")
+p2 = Patient("Jane", "Doe")
+p3 = Patient("Jim", "Doe")
+
+d1 = Doctor("Smith", "Dermatology")
+d2 = Doctor("Johnson", "Neuroproctology")
+
+a1 = Appointment("Nov 16", d1, p1)
+a2 = Appointment("Nov 22", d1, p2)
+a3 = Appointment("Nov 32", d1, p3)
+a4 = Appointment("Nov 64", d2, p1)
+a5 = Appointment("Nov 128", d1, p1)
+a6 = Appointment("Nov 256", d2, p2)
+a7 = Appointment("Nov 512", d2, p1)
+
+
+
+
+##############################
+    
+# we didn't get to this but you can attempt it if you want!
 
 # STUDENT - INSTRUCTOR - COURSE - ASSIGNMENT - SCHOOL RELATIONSHIP ###
 
@@ -97,5 +170,5 @@ class School:
 
     def __repr__(self):
         return f"School(name={self.name})"
-    
+        
 ##############################
